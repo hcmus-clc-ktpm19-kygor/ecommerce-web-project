a6e2810f59df4837fc845159628741baad4a37ec
minhnhat02122001 <lengocminhnhatntp@gmail.com>
2021-12-01 15:55:28 +0700

    edit detail

--- src/app.js
+++ src/app.js
@@ -9,9 +9,6 @@ const usersRouter = require('./routes/users');
 const loginRouter = require('./routes/login');
 const registerRouter = require('./routes/register');
 const confirmationRouter = require('./routes/confirmation');
-const asusRouter = require('./routes/asus');
-const dellRouter = require('./routes/dell');
-const detailRouter = require('./routes/detail');
 
 const accountRouter = require('./components/account/accountRouter');
 const customerRouter = require('./components/customer/customerRouter');
@@ -43,9 +40,6 @@ app.use('/login', loginRouter);
 app.use('/register', registerRouter);
 app.use('/products', productRouter);
 app.use('/confirmation', confirmationRouter);
-app.use('/asus', asusRouter);
-app.use('/dell', dellRouter);
-app.use('/detail', detailRouter);
 app.use('/users', usersRouter);
 
 app.use('/api/account', accountRouter);
