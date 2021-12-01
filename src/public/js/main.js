ca8a191f0ca00c080cdbd8587deaee0f5c7d1fdf
minhnhat02122001 <lengocminhnhatntp@gmail.com>
2021-12-01 20:22:47 +0700

    edit model product

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -89,14 +89,13 @@ $(function() {
     noUiSlider.create(nonLinearSlider, {
         connect: true,
         behaviour: 'tap',
-        start: [ 500, 4000 ],
+        start: [ 500, 2000 ],
         range: {
             // Starting at 500, step the value by 500,
             // until 4000 is reached. From there, step by 1000.
-            'min': [ 0 ],
-            '10%': [ 500, 500 ],
-            '50%': [ 4000, 1000 ],
-            'max': [ 10000 ]
+            'min': [ 500 ],
+            '10%': [ 500, 100 ],
+            'max': [ 2000 ]
         }
     });
   
