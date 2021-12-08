5f94aa685c7f4ae6b6da29e0cad8c1392532c8f9
namworkmc <namworkmc@gmail.com>
2021-12-08 14:37:27 +0700

    merge

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Passport middleware
+// Passport middlewares
 app.use(session({ secret: process.env.SESSION_SECRET_KEY }));
 app.use(passport.initialize());
 app.use(passport.session());
