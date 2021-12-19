87326c49ffcf382ead94492f4c31c7a11908814f
namworkmc <namworkmc@gmail.com>
2021-12-19 12:12:41 +0700

    merge

--- src/api/comment/commentApiRouter.js
+++ src/api/comment/commentApiRouter.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const commentController = require("../../components/comment/commentController");
 
 // GET Method
-router.get('/:id/comments', commentController.getComment);
+// router.get('/:id/comments', commentController.getComment);
+router.get('/comments', commentController.getComment);
 
 // POST Method
-router.post('/:id/comments', commentController.postComment);
+// router.post('/:id/comments', commentController.postComment);
+router.post('/comments', commentController.postComment);
 
 module.exports = router;
