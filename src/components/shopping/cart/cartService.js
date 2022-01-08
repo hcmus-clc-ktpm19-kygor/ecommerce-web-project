1c5ef2fd78ad45ca26a8cb4752729104ad9b1290
namworkmc <namworkmc@gmail.com>
2022-01-09 00:06:42 +0700

    fix không add sản phẩm vào cart được

--- src/components/shopping/cart/cartService.js
+++ src/components/shopping/cart/cartService.js
@@ -1,4 +1,4 @@
-const cartModel = require('./cartModel');
+const cartModel = require("./cartModel");
 const model = require("../../product/productModel");
 const { ObjectId } = require("mongoose").Types;
 
@@ -8,15 +8,17 @@ const { ObjectId } = require("mongoose").Types;
  * @param id id tạm của tài khoản chưa đăng nhập
  * @returns {Promise<void>}
  */
-exports.getCartByGuestId = async function(id) {
-    try {
-        return await cartModel.findOne({
-            guest_id: id,
-        }).lean();
-    } catch (err) {
-        throw err;
-    }
-}
+exports.getCartByGuestId = async function (id) {
+  try {
+    return await cartModel
+      .findOne({
+        guest_id: id,
+      })
+      .lean();
+  } catch (err) {
+    throw err;
+  }
+};
 
 /**
  * Lấy giỏ hàng của các tài khoản đã đăng nhập
@@ -24,30 +26,32 @@ exports.getCartByGuestId = async function(id) {
  * @param id id của account
  * @returns {Promise<void>}
  */
-exports.getCartByUserId = async function(id) {
-    try {
-        return await cartModel.findOne({
-            customer_id: ObjectId.createFromHexString(id),
-        }).lean();
-    } catch (err) {
-        throw err;
-    }
-}
+exports.getCartByUserId = async function (id) {
+  try {
+    return await cartModel
+      .findOne({
+        customer_id: ObjectId.createFromHexString(id),
+      })
+      .lean();
+  } catch (err) {
+    throw err;
+  }
+};
 
 /**
  * Thêm cart mới vào database
  * @param cart
  * @returns {Promise<Document<any, any, unknown> & Require_id<unknown>>}
  */
-exports.insertCart = async function(cart) {
-    try {
-        const newCart = new cartModel(cart);
+exports.insertCart = async function (cart) {
+  try {
+    const newCart = new cartModel(cart);
 
-        return await newCart.save();
-    } catch (err) {
-        throw err;
-    }
-}
+    return await newCart.save();
+  } catch (err) {
+    throw err;
+  }
+};
 
 /**
  * Thêm sp mới vào cart
@@ -55,28 +59,29 @@ exports.insertCart = async function(cart) {
  * @param user_id
  * @returns {Promise<{mess: string}|Query<any, any, {}, any>>}
  */
-exports.insertProductToCart = async function(product_id, user_id) {
-    try {
-        const product = await model.findById(product_id).lean();
-        const small_product = {
-            id: product._id,
-            name: product.name,
-            price: product.price,
-            quantity: 1,
-            image_url: product.image_url,
-        };
+exports.insertProductToCart = async function (product_id, user_id) {
+  try {
+    const product = await model.findById(product_id).lean();
+    const small_product = {
+      id: product._id,
+      name: product.name,
+      price: product.price,
+      quantity: 1,
+      image_url: product.image_url,
+    };
 
-        console.log(small_product);
-        // const cart = await cartModel.findOne({guest_id: guest_id,}).lean();
-        const cart = await cartModel.findOne({
-            customer_id: ObjectId.createFromHexString(user_id),
-        }).lean();
-        cartModel.findOneAndUpdate(
-            {_id : ObjectId.createFromHexString(cart._id) },
-            { push: { products: small_product } },
-        );
-    } catch (err) {
-        throw err;
-    }
-}
-
+    console.log(small_product);
+    // const cart = await cartModel.findOne({guest_id: guest_id,}).lean();
+    const cart = await cartModel
+      .findOne({
+        customer_id: ObjectId.createFromHexString(user_id),
+      })
+      .lean();
+    await cartModel.findOneAndUpdate(
+      { _id: cart._id },
+      { $push: { products: small_product } }
+    );
+  } catch (err) {
+    throw err;
+  }
+};
