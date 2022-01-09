d3ba14e6ca7a40cec02b42b5c95bf910df04b38b
namworkmc <namworkmc@gmail.com>
2022-01-09 17:57:38 +0700

    merge

--- src/app.js
+++ src/app.js
@@ -1,48 +1,51 @@
-require('dotenv').config();
-require('./helpers/helper');
+require("dotenv").config();
+require("./helpers/helper");
 
-const express = require('express');
+const express = require("express");
 
-const path = require('path');
+const path = require("path");
 const passport = require("./config/passport.config");
-const flash = require('connect-flash');
+const flash = require("connect-flash");
 
-const createError = require('http-errors');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const session = require('express-session');
-const methodOverride = require('method-override');
+const createError = require("http-errors");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const session = require("express-session");
+const methodOverride = require("method-override");
+const cors = require("cors");
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const confirmationRouter = require('./routes/confirmation');
-const productRouter = require('./components/product/productRouter');
-const authRouter = require('./components/auth/authRouter');
-const accountRouter = require('./components/account/accountRouter');
-const cartRouter = require('./components/shopping/cart/cartRouter')
-const apiRouter = require('./api/apiRouter');
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const confirmationRouter = require("./routes/confirmation");
+const productRouter = require("./components/product/productRouter");
+const authRouter = require("./components/auth/authRouter");
+const accountRouter = require("./components/account/accountRouter");
+const cartRouter = require("./components/shopping/cart/cartRouter");
+const apiRouter = require("./api/apiRouter");
 
-
-const loggedInUserGuard = require('./middlewares/loggedInUserGuard');
-const userIdMiddleware = require('./middlewares/userIdMiddleware');
+const loggedInUserGuard = require("./middlewares/loggedInUserGuard");
+const userIdMiddleware = require("./middlewares/userIdMiddleware");
 
 // try to connect to database
-const db = require('./config/database.config');
+const db = require("./config/database.config");
 db.connect();
 
 const app = express();
 
 // view engine setup
-app.set('views', [path.join(__dirname, 'views'), path.join(__dirname, "components")]);
-app.set('view engine', 'hbs');
+app.set("views", [
+  path.join(__dirname, "views"),
+  path.join(__dirname, "components"),
+]);
+app.set("view engine", "hbs");
 
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(methodOverride('_method', { methods: ['POST', 'GET'] }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(methodOverride("_method", { methods: ["POST", "GET"] }));
+app.use(cors());
 
 // Passport middlewares
 app.use(session({ secret: process.env.SESSION_SECRET_KEY }));
@@ -53,34 +56,34 @@ app.use(flash());
 app.use(function (req, res, next) {
   res.locals.user = req.user;
   next();
-})
+});
 
 app.use(userIdMiddleware);
 
 // Router middleware
-app.use('/', indexRouter);
-app.use('/', authRouter);
-app.use('/account', loggedInUserGuard ,accountRouter);
-app.use('/products', productRouter);
-app.use('/confirmation', loggedInUserGuard, confirmationRouter);
-app.use('/users', usersRouter);
-app.use('/cart', cartRouter);
-app.use('/api', apiRouter);
+app.use("/", indexRouter);
+app.use("/", authRouter);
+app.use("/account", loggedInUserGuard, accountRouter);
+app.use("/products", productRouter);
+app.use("/confirmation", loggedInUserGuard, confirmationRouter);
+app.use("/users", usersRouter);
+app.use("/cart", cartRouter);
+app.use("/api", apiRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
