a3049e36372d80fe1267b6bb70c4cddb98c21606
minhnhat02122001 <lengocminhnhatntp@gmail.com>
2022-10-31 10:31:10 +0700

    edit home page

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const productService = require('../components/product/productService');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
+router.get('/',  async function(req, res, next) {
+  const products = await productService.getAllProduct();
+  res.render('index', {products});
 });
 
 module.exports = router;
